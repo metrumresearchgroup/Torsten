a22accbc88b2a225ee29c223877aab363d090522
Charles Margossian <charlesm@metrumrg.com>
2016-07-07 22:44:20 -0400

    Added files with compartment functions
    These are the files that define the functions the user will be able to call directly in a Stan file.

---
+++ torsten/GeneralCptModel_rk45.hpp
@@ -0,0 +1,93 @@
+// version 0.8
+
+/**
+ * Computes the predicted amounts in each compartment at each event
+ * for a general compartment model, defined by a system of ordinary
+ * differential equations. Uses the stan::math::integrate_ode_rk45 
+ * function. 
+ *
+ * <b>Warning:</b> This prototype does not handle steady state events. 
+ *
+ * @tparam T0 type of scalars for the model parameters.
+ * @tparam T1 type of scalar for time of events. 
+ * @tparam T2 type of scalar for amount at each event.
+ * @tparam T3 type of scalar for rate at each event.
+ * @tparam T4 type of scalar for inter-dose inteveral at each event.
+ * @tparam F type of ODE system function.
+ * @param[in] f functor for base ordinary differential equation that defines 
+ *            compartment model.
+ * @param[in] nCmt number of compartments in model
+ * @param[in] pMatrix parameters at each event
+ * @param[in] time times of events  
+ * @param[in] amt amount at each event
+ * @param[in] rate rate at each event
+ * @param[in] ii inter-dose interval at each event
+ * @param[in] evid event identity: 
+ *                    (0) observation 
+ *                    (1) dosing
+ *                    (2) other 
+ *                    (3) reset 
+ *                    (4) reset AND dosing 
+ * @param[in] cmt compartment number at each event 
+ * @param[in] addl additional dosing at each event 
+ * @param[in] ss steady state approximation at each event (0: no, 1: yes)
+ * @param[in] rel_tol relative tolerance for the Boost ode solver 
+ * @param[in] abs_tol absolute tolerance for the Boost ode solver
+ * @param[in] max_num_steps maximal number of steps to take within 
+ *            the Boost ode solver 
+ * @return a matrix with predicted amount in each compartment 
+ *         at each event. 
+ */
+
+#include <stan/model/model_header.hpp>
+#include <Eigen/Dense>
+#include <boost/math/tools/promotion.hpp>
+#include "PKModel/PKModel.hpp"
+using std::vector;
+using Eigen::Dynamic;
+using Eigen::Matrix;
+using boost::math::tools::promote_args;
+
+
+template <typename T0, typename T1, typename T2, typename T3, typename T4, typename F> 
+Matrix <typename promote_args<T0, T1, T2, T3, T4>::type, Dynamic, Dynamic> 
+generalCptModel_rk45(const F& f,
+                     const int nCmt,
+			               const vector< Matrix<T0, Dynamic, 1> >& pMatrix, 
+			               const vector<T1>& time,
+			               const vector<T2>& amt,
+			               const vector<T3>& rate,
+			               const vector<T4>& ii,
+			               const vector<int>& evid,
+			               const vector<int>& cmt,
+			               const vector<int>& addl,
+			               const vector<int>& ss,
+			               double rel_tol = 1e-10,
+                     double abs_tol = 1e-10,
+                     long int max_num_steps = 1e8)
+{
+
+	//Define class of model
+	int nParameters, F1Index, tlag1Index;
+	nParameters = pMatrix[0].rows();
+	F1Index = nParameters - 2*nCmt;
+	tlag1Index = nParameters - nCmt;
+	PKModel model(nParameters, F1Index, tlag1Index, nCmt);
+	static const char* function("generalCptModel_rk45");
+	Matrix <typename promote_args<T0, T1, T2, T3, T4>::type, Dynamic, Dynamic> pred;
+	
+	pmetricsCheck(pMatrix, time, amt, rate, ii, evid, cmt, addl, ss, function, model);
+	
+  //Construct Pred functions for the model.
+  Pred1_structure new_Pred1("GeneralCptModel");
+  PredSS_structure new_PredSS("GeneralCptModel");
+  Pred1 = new_Pred1;
+  PredSS = new_PredSS; // WARNING: PredSS returns an error message and aborts program
+  pmetrics_solver_structure new_pmetrics_solver(rel_tol, abs_tol, max_num_steps, "rk45");
+  pmetrics_solver = new_pmetrics_solver;
+        
+  pred = Pred(pMatrix, time, amt, rate, ii, evid, cmt, addl, ss, model, f);
+        
+  return pred;
+}
+
---
+++ torsten/PKModelTwoCpt.hpp
@@ -0,0 +1,74 @@
+// version 0.8
+
+/**
+ * Computes the predicted amounts in each compartment at each event
+ * for a two compartments model with first oder absorption. 
+ * *
+ * @tparam T0 type of scalars for the model parameters.
+ * @tparam T1 type of scalar for time of events. 
+ * @tparam T2 type of scalar for amount at each event.
+ * @tparam T3 type of scalar for rate at each event.
+ * @tparam T4 type of scalar for inter-dose inteveral at each event.
+ * @param[in] pMatrix parameters at each event
+ * @param[in] time times of events  
+ * @param[in] amt amount at each event
+ * @param[in] rate rate at each event
+ * @param[in] ii inter-dose interval at each event
+ * @param[in] evid event identity: 
+ *                    (0) observation 
+ *                    (1) dosing
+ *                    (2) other 
+ *                    (3) reset 
+ *                    (4) reset AND dosing 
+ * @param[in] cmt compartment number at each event 
+ * @param[in] addl additional dosing at each event 
+ * @param[in] ss steady state approximation at each event (0: no, 1: yes)
+ * @return a matrix with predicted amount in each compartment 
+ *         at each event. 
+ */
+
+#include <stan/model/model_header.hpp>
+#include <Eigen/Dense>
+#include "PKModel/PKModel.hpp"
+#include <boost/math/tools/promotion.hpp>
+using std::vector;
+using Eigen::Dynamic;
+using Eigen::Matrix;
+using boost::math::tools::promote_args;
+
+template <typename T0, typename T1, typename T2, typename T3, typename T4> 
+Matrix <typename promote_args<T0, T1, T2, T3, T4>::type, Dynamic, Dynamic> 
+PKModelTwoCpt(const vector< Matrix<T0, Dynamic, 1> >& pMatrix, 
+			  const vector<T1>& time,
+			  const vector<T2>& amt,
+			  const vector<T3>& rate,
+			  const vector<T4>& ii,
+			  const vector<int>& evid,
+			  const vector<int>& cmt,
+			  const vector<int>& addl,
+			  const vector<int>& ss) 							 
+{
+	PKModel model("TwoCptModel"); //Define class of model
+  static const char* function("PKModelTwoCpt");
+  
+  pmetricsCheck(pMatrix, time, amt, rate, ii, evid, cmt, addl, ss, function, model);
+  for(int i=0; i<pMatrix.size(); i++) {
+    stan::math::check_positive_finite(function, "PK parameter CL", pMatrix[i](0,0));
+    stan::math::check_positive_finite(function, "PK parameter Q", pMatrix[i](1,0));
+    stan::math::check_positive_finite(function, "PK parameter V2", pMatrix[i](2,0));
+    stan::math::check_positive_finite(function, "PK parameter V3", pMatrix[i](3,0));
+    stan::math::check_positive_finite(function, "PK parameter ka", pMatrix[i](4,0));
+  }  
+  
+  //Construct Pred functions for the model.
+  Pred1_structure new_Pred1("TwoCptModel");
+  PredSS_structure new_PredSS("TwoCptModel");
+  Pred1 = new_Pred1;
+  PredSS = new_PredSS;
+        
+  Matrix <typename promote_args<T0, T1, T2, T3, T4>::type, Dynamic, Dynamic> pred;
+  pred = Pred(pMatrix, time, amt, rate, ii, evid, cmt, addl, ss, model, dummy_ode());
+
+  return pred;
+}
+
