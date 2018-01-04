7112bcece00106813f2d6963501114ab3687ae79
Yi Zhang <yizhang.altair@gmail.com>
2018-01-04 17:33:08 -0500

    move univariate_integral into torsten

--- test/unit/math/torsten/univariate_integral_test.cpp
+++ test/unit/math/torsten/univariate_integral_test.cpp
@@ -65,11 +65,10 @@ TEST(univariate_integral, const_example) {
   univar_integral_const_fun f;
   double t0 {0.0};
   double t1 {2.5};
-  int n {100};
   std::vector<double> theta;
   theta.push_back(2.0);
 
-  EXPECT_NEAR(5.0, stan::math::univariate_integral(f, theta, t0, t1, n), 1e-5);
+  EXPECT_NEAR(5.0, stan::math::univariate_integral(f, theta, t0, t1), 1e-5);
 }
 
 TEST(univariate_integral, linear_example) {
@@ -77,11 +76,10 @@ TEST(univariate_integral, linear_example) {
   univar_integral_linear_fun f;
   double t0 {0.0};
   double t1 {2.5};
-  int n {100};
   std::vector<double> theta;
   theta.push_back(1.2);
 
-  EXPECT_NEAR(3.75, stan::math::univariate_integral(f, theta, t0, t1, n), 1e-5);
+  EXPECT_NEAR(3.75, stan::math::univariate_integral(f, theta, t0, t1), 1e-5);
 }
 
 TEST(univariate_integral, quad_example) {
@@ -89,9 +87,8 @@ TEST(univariate_integral, quad_example) {
   univar_integral_quad_fun f;
   double t0 {0.0};
   double t1 {0.4};
-  int n {10};
   std::vector<double> theta {2.3, 2.0, 1.5};
 
-  EXPECT_NEAR(1.112, stan::math::univariate_integral(f, theta, t0, t1, n), 1e-5);
+  EXPECT_NEAR(1.112, stan::math::univariate_integral(f, theta, t0, t1), 1e-5);
 }
 
