a332b28c3dbba36be53735d2f74af25a1c6c3df7
Yi Zhang <yiz@metrumrg.com>
2018-01-04 22:48:11 -0500

    use ode_simple_functor to do univar integral

--- stan/math/prim/arr.hpp
+++ stan/math/prim/arr.hpp
@@ -31,7 +31,6 @@
 #include <stan/math/prim/arr/fun/sort_desc.hpp>
 #include <stan/math/prim/arr/fun/sub.hpp>
 #include <stan/math/prim/arr/fun/sum.hpp>
-#include <stan/math/prim/arr/fun/univariate_integral.hpp>
 #include <stan/math/prim/arr/fun/value_of.hpp>
 #include <stan/math/prim/arr/fun/value_of_rec.hpp>
 
