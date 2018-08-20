75b95745aee68874439f0bbfdb38d6983bd0db73
Yi Zhang <yiz@metrumrg.com>
2018-08-20 16:17:20 -0400

    merge refactor's twocpt model/solver

---
+++ stan/math/torsten/pk_twocpt_model.hpp
@@ -0,0 +1,199 @@
+#ifndef STAN_MATH_TORSTEN_TWOCPT_MODEL_HPP
+#define STAN_MATH_TORSTEN_TWOCPT_MODEL_HPP
+
+#include <stan/math/torsten/torsten_def.hpp>
+
+namespace refactor {
+
+  using boost::math::tools::promote_args;
+
+  /**
+   * standard two compartment PK ODE functor
+   */
+  struct PKTwoCptODE {
+  /**
+   * standard two compartment PK ODE RHS function
+   * @tparam T0 t type
+   * @tparam T1 initial condition type
+   * @tparam T2 parameter type
+   * @tparam T3 real data/rate type
+   * @param t type
+   * @param x initial condition type
+   * @param parms parameters
+   * @param rate dosing rate
+   * @param dummy dummy
+   */
+    template <typename T0, typename T1, typename T2, typename T3>
+    inline
+    std::vector<typename boost::math::tools::promote_args<T0, T1, T2, T3>::type>
+    operator()(const T0& t,
+               const std::vector<T1>& x,
+               const std::vector<T2>& parms,
+               const std::vector<T3>& rate,
+               const std::vector<int>& dummy, std::ostream* pstream__) const {
+      typedef typename boost::math::tools::promote_args<T0, T1, T2, T3>::type scalar;
+
+      scalar
+        CL = parms[0],
+        Q = parms[1],
+        V1 = parms[2],
+        V2 = parms[3],
+        ka = parms[4],
+        k10 = CL / V1,
+        k12 = Q / V1,
+        k21 = Q / V2;
+
+      std::vector<scalar> y(3, 0);
+      y[0] = -ka * x[0];
+      y[1] = ka * x[0] - (k10 + k12) * x[1] + k21 * x[2];
+      y[2] = k12 * x[1] - k21 * x[2];
+
+      return y;
+    }
+  };
+
+  /**
+   * two-compartment PK model. The static memebers provide
+   * universal information, i.e. nb. of compartments,
+   * nb. of parameters, and the RHS functor.
+   *
+   * @tparam T_time t type
+   * @tparam T_init initial condition type
+   * @tparam T_rate dosing rate type
+   * @tparam T_par PK parameters type
+   */
+  template<typename T_time, typename T_init, typename T_rate, typename T_par>
+  class PKTwoCptModel {
+    const T_time &t0_;
+    const Eigen::Matrix<T_init, 1, Eigen::Dynamic>& y0_;
+    const std::vector<T_rate> &rate_;
+    const T_par &CL_;
+    const T_par &Q_;
+    const T_par &V2_;
+    const T_par &V3_;
+    const T_par &ka_;
+    const T_par k10_;
+    const T_par k12_;
+    const T_par k21_;
+    const T_par ksum_;
+    const std::vector<T_par> alpha_;
+
+  public:
+    static constexpr int Ncmt = 3;
+    static constexpr int Npar = 5;
+    static constexpr PKTwoCptODE f_ = PKTwoCptODE();    // can be solved by gen ode solver
+
+    using scalar_type = typename promote_args<T_time, T_rate, T_par, T_init>::type;
+    using aug_par_type = typename promote_args<T_rate, T_par, T_init>::type;
+    using init_type   = T_init;
+    using time_type   = T_time;
+    using par_type    = T_par;
+    using rate_type   = T_rate;
+    using f_type      = PKTwoCptODE;
+
+  /**
+   * Two-compartment PK model constructor
+   *
+   * @param t0 initial time
+   * @param y0 initial condition
+   * @param rate dosing rate
+   * @param par model parameters
+   * @param CL clearance
+   * @param Q distributed amt
+   * @param V2 central cpt vol
+   * @param V3 peri cpt vol
+   * @param ka absorption
+   */
+    PKTwoCptModel(const T_time& t0, const Eigen::Matrix<T_init, 1, Eigen::Dynamic>& y0,
+                  const std::vector<T_rate> &rate,
+                  const T_par& CL,
+                  const T_par& Q,
+                  const T_par& V2,
+                  const T_par& V3,
+                  const T_par& ka) :
+      t0_(t0),
+      y0_(y0),
+      rate_(rate),
+      CL_(CL),
+      Q_(Q),
+      V2_(V2),
+      V3_(V3),
+      ka_(ka),
+      k10_(CL_ / V2_),
+      k12_(Q_ / V2_),
+      k21_(Q_ / V3_),
+      ksum_(k10_ + k12_ + k21_),
+      alpha_{0.5 * (ksum_ + sqrt(ksum_ * ksum_ - 4 * k10_ * k21_)),
+        0.5 * (ksum_ - sqrt(ksum_ * ksum_ - 4 * k10_ * k21_)),
+        ka_}
+    {}
+
+  /**
+   * two-Compartment PK model constructor
+   * FIXME need to remove parameter as this is for linode only.
+   *
+   * @tparam T_mp parameters class
+   * @tparam Ts parameter types
+   * @param t0 initial time
+   * @param y0 initial condition
+   * @param rate dosing rate
+   * @param par model parameters
+   * @param parameter ModelParameter type
+   */
+    template<template<typename...> class T_mp, typename... Ts>
+    PKTwoCptModel(const T_time& t0,
+                  const Eigen::Matrix<T_init, 1, Eigen::Dynamic>& y0,
+                  const std::vector<T_rate> &rate,
+                  const std::vector<T_par> & par,
+                  const T_mp<Ts...> &parameter) :
+      PKTwoCptModel(t0, y0, rate, par[0], par[1], par[2], par[3], par[4])
+    {}
+
+  /**
+   * two-compartment PK model constructor
+   *
+   * @param t0 initial time
+   * @param y0 initial condition
+   * @param rate dosing rate
+   * @param par model parameters
+   */
+    PKTwoCptModel(const T_time& t0,
+                  const Eigen::Matrix<T_init, 1, Eigen::Dynamic>& y0,
+                  const std::vector<T_rate> &rate,
+                  const std::vector<T_par> & par) :
+      PKTwoCptModel(t0, y0, rate, par[0], par[1], par[2], par[3], par[4])
+    {}
+
+  /**
+   * two-compartment PK model get methods
+   */
+    const T_time              & t0()      const { return t0_;    }
+    const PKRec<T_init>    & y0()         const { return y0_;    }
+    const std::vector<T_rate> & rate()    const { return rate_;  }
+    const T_par               & CL()      const { return CL_;    }
+    const T_par               & Q()       const { return Q_;     }
+    const T_par               & V2()      const { return V2_;    }
+    const T_par               & V3()      const { return V3_;    }
+    const T_par               & ka()      const { return ka_;    }
+    const T_par               & k10()     const { return k10_;   }
+    const T_par               & k12()     const { return k12_;   }
+    const T_par               & k21()     const { return k21_;   }
+    const std::vector<T_par>    par()     const { return {CL_, Q_, V2_, V3_, ka_}; }
+    const std::vector<T_par>  & alpha()   const { return alpha_; }
+    const PKTwoCptODE         & f()       const { return f_;     }
+    const int                 & ncmt ()   const { return Ncmt;   }
+
+  };
+
+  template<typename T_time, typename T_init, typename T_rate, typename T_par>
+  constexpr int PKTwoCptModel<T_time, T_init, T_rate, T_par>::Ncmt;
+
+  template<typename T_time, typename T_init, typename T_rate, typename T_par>
+  constexpr int PKTwoCptModel<T_time, T_init, T_rate, T_par>::Npar;
+
+  template<typename T_time, typename T_init, typename T_rate, typename T_par>
+  constexpr PKTwoCptODE PKTwoCptModel<T_time, T_init, T_rate, T_par>::f_;
+
+}
+
+#endif
---
+++ test/unit/math/torsten/pk_twocpt_model_test.cpp
@@ -0,0 +1,121 @@
+#include <stan/math.hpp>
+#include <stan/math/rev/core.hpp>
+#include <test/unit/math/rev/mat/fun/util.hpp>
+#include <test/unit/math/torsten/pk_twocpt_model_test_fixture.hpp>
+#include <test/unit/util.hpp>
+#include <gtest/gtest.h>
+
+TEST_F(TorstenTwoCptModelTest, rate_dbl) {
+  using stan::math::var;
+  using stan::math::to_var;
+  using refactor::PKTwoCptModel;
+  using refactor::PKODERateAdaptor;
+  using stan::math::integrate_ode_bdf;
+  using torsten::dsolve::pk_integrate_ode_bdf;
+
+  rate[0] = 1200;
+  rate[1] = 200;
+  rate[2] = 300;
+  using model_t = PKTwoCptModel<double, double, double, double>;
+  model_t model(t0, y0, rate, CL, Q, V2, V3, ka);
+  std::vector<double> yvec(y0.data(), y0.data() + y0.size());
+  PKODERateAdaptor<model_t> rate_adaptor(model);
+
+  std::vector<double> y =
+    rate_adaptor.model().f()(t0, yvec, model.par(), rate, x_i, msgs);
+  EXPECT_FLOAT_EQ(y[0], rate[0]);
+  EXPECT_FLOAT_EQ(y[1], rate[1]);
+  EXPECT_FLOAT_EQ(y[2], rate[2]);
+}
+
+TEST_F(TorstenTwoCptModelTest, rate_var) {
+  using stan::math::var;
+  using stan::math::to_var;
+  using refactor::PKTwoCptModel;
+  using refactor::PKODERateAdaptor;
+  using stan::math::integrate_ode_bdf;
+  using torsten::dsolve::pk_integrate_ode_bdf;
+
+  rate[0] = 1200;
+  rate[1] = 200;
+  rate[2] = 100;
+  var CLv = to_var(CL);
+  var Qv  = to_var(Q);
+  var V2v = to_var(V2);
+  var V3v = to_var(V3);
+  var kav = to_var(ka);
+  std::vector<stan::math::var> rate_var{to_var(rate)};
+  using model_t = PKTwoCptModel<double, double, var, var>;
+  model_t model(t0, y0, rate_var, CLv, Qv, V2v, V3v, kav);
+  std::vector<double> yvec(y0.data(), y0.data() + y0.size());
+  PKODERateAdaptor<model_t> rate_adaptor(model);
+
+  EXPECT_EQ(rate_adaptor.model().par().size(),
+            model.par().size() + model.rate().size());
+  std::vector<var> y =
+    rate_adaptor.model().f()(t0, yvec, rate_adaptor.model().par(),
+                             x_r, x_i, msgs);
+  EXPECT_FLOAT_EQ(y[0].val(), rate[0]);
+  EXPECT_FLOAT_EQ(y[1].val(), rate[1]);
+  EXPECT_FLOAT_EQ(y[2].val(), rate[2]);
+}
+
+TEST_F(TorstenTwoCptModelTest, twocpt_solver) {
+  using stan::math::var;
+  using stan::math::to_var;
+  using refactor::PKTwoCptModel;
+  using refactor::PKTwoCptModelSolver;
+  using refactor::PKODERateAdaptor;
+  using stan::math::integrate_ode_bdf;
+  using torsten::dsolve::pk_integrate_ode_bdf;
+
+  rate[0] = 1200;
+  rate[1] = 200;
+  rate[2] = 100;
+  y0[0] = 150;
+  y0[1] = 50;
+  y0[2] = 60;
+  ts[0] = 10.0;
+  ts.resize(1);
+  var CLv = to_var(CL);
+  var Qv  = to_var(Q);
+  var V2v = to_var(V2);
+  var V3v = to_var(V3);
+  var kav = to_var(ka);
+  std::vector<var> theta{CLv, Qv, V2v, V3v, kav};
+  std::vector<stan::math::var> rate_var{to_var(rate)};
+  using model_t = PKTwoCptModel<double, double, var, var>;
+  model_t model(t0, y0, rate_var, CLv, Qv, V2v, V3v, kav);
+  std::vector<double> yvec(y0.data(), y0.data() + y0.size());
+  PKODERateAdaptor<model_t> rate_adaptor(model);
+
+  auto y1 = pk_integrate_ode_bdf(rate_adaptor.model().f(),
+                                 yvec, t0, ts,
+                                 rate_adaptor.model().par(),
+                                 x_r, x_i, msgs);
+  auto y2 = PKTwoCptModelSolver::solve(model, ts[0]);
+  EXPECT_FLOAT_EQ(y1[0][0].val(), y2(0).val());
+  EXPECT_FLOAT_EQ(y1[0][1].val(), y2(1).val());
+  EXPECT_FLOAT_EQ(y1[0][2].val(), y2(2).val());
+
+  std::vector<double> g1, g2;
+  for (int i = 0; i < y0.size(); ++i) {
+    stan::math::set_zero_all_adjoints();    
+    y1[0][i].grad(theta, g1);
+    stan::math::set_zero_all_adjoints();    
+    y2(i).grad(theta, g2);
+    for (size_t j = 0; j < theta.size(); ++j) {
+      EXPECT_FLOAT_EQ(g1[j], g2[j]);
+    }
+  }
+
+  for (int i = 0; i < y0.size(); ++i) {
+    stan::math::set_zero_all_adjoints();    
+    y1[0][i].grad(rate_var, g1);
+    stan::math::set_zero_all_adjoints();    
+    y2(i).grad(rate_var, g2);
+    for (size_t j = 0; j < rate.size(); ++j) {
+      EXPECT_FLOAT_EQ(g1[j], g2[j]);
+    }
+  }
+}
