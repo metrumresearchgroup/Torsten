cea95906c9104f7cde75f1356a12a4ab24877140
Yi Zhang <yiz@metrumrg.com>
2019-05-07 11:08:57 -0700

    rm math

--- cmdstan/stan/lib/stan_math/stan/math/torsten/dsolve.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef STAN_MATH_TORSTEN_DSOLVE_HPP
-#define STAN_MATH_TORSTEN_DSOLVE_HPP
-
-#include <stan/math/torsten/dsolve/pmx_integrate_dae.hpp>
-#include <stan/math/torsten/dsolve/pmx_integrate_ode_adams.hpp>
-#include <stan/math/torsten/dsolve/pmx_integrate_ode_bdf.hpp>
-
-#endif
--- cmdstan/stan/lib/stan_math/test/unit/math/prim/scal/meta/contains_fvar_test.cpp
+++
@@ -1,8 +0,0 @@
-#include <stan/math/prim/scal.hpp>
-#include <gtest/gtest.h>
-#include <boost/type_traits.hpp>
-
-TEST(MetaTraits, containsFvar) {
-  using stan::contains_fvar;
-  EXPECT_FALSE(contains_fvar<double>::value);
-}
