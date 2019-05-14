1574684ceb304f4bef0527bb09f144fc7072108d
yiz <yiz@metrumrg.com>
2019-05-14 10:25:04 -0700

    remove math

--- cmdstan/stan/lib/stan_math/test/unit/multiple_translation_units_test.cpp
+++
@@ -1,6 +0,0 @@
-#include <gtest/gtest.h>
-
-TEST(multiple_translation_units, compile) {
-  SUCCEED() << "this test compiling indicates that compiling the math library "
-            << "with multiple translation units is ok.";
-}
