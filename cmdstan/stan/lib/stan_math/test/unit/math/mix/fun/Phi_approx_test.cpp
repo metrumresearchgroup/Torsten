4f12acf097b61c4f6b15e7aa6d4ef457995f7fd5
Yi Zhang <yiz@metrumrg.com>
2020-10-26 09:36:41 -0700

    rm math

--- cmdstan/stan/lib/stan_math/test/unit/math/mix/fun/Phi_approx_test.cpp
+++
@@ -1,7 +0,0 @@
-#include <test/unit/math/test_ad.hpp>
-
-TEST(mathMixMatFun, PhiApprox) {
-  auto f = [](const auto& x1) { return stan::math::Phi_approx(x1); };
-  stan::test::expect_common_unary_vectorized(f);
-  stan::test::expect_unary_vectorized(f, -3.0, 1, 1.3, 3);
-}
--- cmdstan/stan/lib/stan_math/test/unit/math/mix/fun/inv_Phi_test.cpp
+++
@@ -1,9 +0,0 @@
-#include <test/unit/math/test_ad.hpp>
-
-TEST(mathMixMatFun, invPhi) {
-  auto f = [](const auto& x1) { return stan::math::inv_Phi(x1); };
-  stan::test::expect_common_unary_vectorized(f);
-  stan::test::expect_unary_vectorized(f, 0.02425, 0.97575);  // breakpoints
-  stan::test::expect_unary_vectorized(f, -100.25, -2, 0.01, 0.1, 0.98, 0.5,
-                                      2.0);
-}
