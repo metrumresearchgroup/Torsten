4c2e3f0a924d52d6504f52c8cffb77749d70fdc2
Yi Zhang <yiz@metrumrg.com>
2020-10-29 00:36:45 -0700

    rm math

--- cmdstan/stan/lib/stan_math/test/unit/math/mix/core/operator_less_than_or_equal_test.cpp
+++
@@ -1,6 +0,0 @@
-#include <test/unit/math/test_ad.hpp>
-
-TEST(mathMixCore, operatorLessThanOrEqual) {
-  auto f = [](const auto& x1, const auto& x2) { return x1 <= x2; };
-  stan::test::expect_common_comparison(f);
-}
--- cmdstan/stan/lib/stan_math/test/unit/math/mix/fun/conj_test.cpp
+++
@@ -1,8 +0,0 @@
-#include <test/unit/math/test_ad.hpp>
-#include <complex>
-#include <vector>
-
-TEST(mixScalFun, conj) {
-  auto f = [](const auto& x) { return conj(x); };
-  stan::test::expect_complex_common(f);
-}
