4e8a098878d92e8239d3ffc0774d1b8055c8dda1
Yi Zhang <yiz@metrumrg.com>
2020-11-13 22:16:44 -0800

    Squashed 'cmdstan/stan/lib/stan_math/stan/math/torsten/' content from commit 79cd4a07b9
    git-subtree-dir: cmdstan/stan/lib/stan_math/stan/math/torsten
    git-subtree-split: 79cd4a07b90814ebfe88af827294ea6362756f51

---
+++ torsten.hpp
@@ -0,0 +1,19 @@
+#ifndef STAN_MATH_TORSTEN_HPP
+#define STAN_MATH_TORSTEN_HPP
+
+#include <stan/math/torsten/pmx_solve_bdf.hpp>
+#include <stan/math/torsten/pmx_solve_rk45.hpp>
+#include <stan/math/torsten/pmx_solve_adams.hpp>
+#include <stan/math/torsten/pmx_solve_linode.hpp>
+#include <stan/math/torsten/pmx_solve_onecpt_rk45.hpp>
+#include <stan/math/torsten/pmx_solve_onecpt_bdf.hpp>
+#include <stan/math/torsten/pmx_solve_twocpt_rk45.hpp>
+#include <stan/math/torsten/pmx_solve_twocpt_bdf.hpp>
+#include <stan/math/torsten/pmx_solve_onecpt.hpp>
+#include <stan/math/torsten/pmx_solve_twocpt.hpp>
+#include <stan/math/torsten/linear_interpolation.hpp>
+#include <stan/math/torsten/univariate_integral.hpp>
+
+#include <stan/math/torsten/dsolve.hpp>
+
+#endif
