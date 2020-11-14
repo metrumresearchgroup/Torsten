86ec5deed3faa38f0542fb4db8a7fe837897b356
Yi Zhang <yiz@metrumrg.com>
2020-11-13 22:16:47 -0800

    Squashed 'stanc3/' content from commit 0af73986b7
    git-subtree-dir: stanc3
    git-subtree-split: 0af73986b7700e1fbdab9e61e3b363b74b5b9d39

---
+++ test/unit/Stan_math_code_gen_tests.ml
@@ -0,0 +1,55 @@
+open Middle
+open Stan_math_backend
+open Core_kernel
+open Fmt
+open Stan_math_code_gen
+
+let%expect_test "udf" =
+  let with_no_loc stmt =
+    Stmt.Fixed.{pattern= stmt; meta= Locations.no_span_num}
+  in
+  let w e = Expr.{Fixed.pattern= e; meta= Typed.Meta.empty} in
+  let pp_fun_def_w_rs a b = pp_fun_def a b String.Set.empty String.Set.empty in
+  { fdrt= None
+  ; fdname= "sars"
+  ; fdargs= [(DataOnly, "x", UMatrix); (AutoDiffable, "y", URowVector)]
+  ; fdbody=
+      Stmt.Fixed.Pattern.Return
+        (Some
+           (w @@ FunApp (StanLib, "add", [w @@ Var "x"; w @@ Lit (Int, "1")])))
+      |> with_no_loc |> List.return |> Stmt.Fixed.Pattern.Block |> with_no_loc
+      |> Some
+  ; fdloc= Location_span.empty }
+  |> strf "@[<v>%a" pp_fun_def_w_rs
+  |> print_endline ;
+  [%expect
+    {|
+    template <typename T1__>
+    void
+    sars(const Eigen::Matrix<double, -1, -1>& x,
+         const Eigen::Matrix<T1__, 1, -1>& y, std::ostream* pstream__) {
+      using local_scalar_t__ = stan::promote_args_t<T1__>;
+      const static bool propto__ = true;
+      (void) propto__;
+      local_scalar_t__ DUMMY_VAR__(std::numeric_limits<double>::quiet_NaN());
+      (void) DUMMY_VAR__;  // suppress unused var warning
+
+      try {
+        return add(x, 1);
+      } catch (const std::exception& e) {
+        stan::lang::rethrow_located(e, locations_array__[current_statement__]);
+          // Next line prevents compiler griping about no return
+          throw std::runtime_error("*** IF YOU SEE THIS, PLEASE REPORT A BUG ***");
+      }
+
+    }
+
+    struct sars_functor__ {
+    template <typename T1__>
+    void
+    operator()(const Eigen::Matrix<double, -1, -1>& x,
+               const Eigen::Matrix<T1__, 1, -1>& y, std::ostream* pstream__)  const
+    {
+    return sars(x, y, pstream__);
+    }
+    }; |}]
