3c2189e2657ac1d040582c1e9c263aabe102b378
Yi Zhang <yiz@metrumrg.com>
2020-11-24 00:02:45 -0800

    rm math

--- cmdstan/stan/lib/stan_math/test/unit/math/mix/fun/square_test.cpp
+++
@@ -1,8 +0,0 @@
-#include <test/unit/math/test_ad.hpp>
-
-TEST(mathMixMatFun, square) {
-  auto f = [](const auto& x1) { return stan::math::square(x1); };
-  stan::test::expect_common_unary_vectorized(f);
-  stan::test::expect_unary_vectorized(f, -2.6, -1.0, -0.5, -0.2, 0.5, 1.3, 3, 5,
-                                      1e5);
-}
