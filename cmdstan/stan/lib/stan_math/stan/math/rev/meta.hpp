d45f252ebd434d284c8d12ab637fb4dadd170307
Yi Zhang <yiz@metrumrg.com>
2020-11-23 22:41:58 -0800

    rm math

--- cmdstan/stan/lib/stan_math/stan/math/rev/meta.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef STAN_MATH_REV_META_HPP
-#define STAN_MATH_REV_META_HPP
-
-#include <stan/math/prim/meta.hpp>
-#include <stan/math/rev/meta/is_var.hpp>
-#include <stan/math/rev/meta/partials_type.hpp>
-
-#endif
