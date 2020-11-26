d5311e0754430b034d9d9e9fdc036282673931f2
Yi Zhang <yiz@metrumrg.com>
2020-11-26 00:33:30 -0800

    rm math

--- cmdstan/stan/lib/stan_math/test/unit/math/prim/fun/cos_test.cpp
+++
@@ -1,7 +0,0 @@
-#include <stan/math/prim.hpp>
-#include <test/unit/math/test_ad.hpp>
-
-TEST(primScalFun, cos) {
-  stan::test::expect_common_prim([](auto x) { return std::cos(x); },
-                                 [](auto x) { return stan::math::cos(x); });
-}
