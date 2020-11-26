e1b16d9e56dff478710f3adcbd4ccb6a666d6f5f
Yi Zhang <yiz@metrumrg.com>
2020-11-26 00:33:38 -0800

    Squashed 'cmdstan/stan/' content from commit 8387f4c7e3
    git-subtree-dir: cmdstan/stan
    git-subtree-split: 8387f4c7e33755b6ad833107ea750aee6d1b6997

---
+++ src/stan/mcmc/cross_chain/mpi_var_adaptation.hpp
@@ -0,0 +1,70 @@
+#ifndef STAN_MCMC_MPI_VAR_ADAPTATION_HPP
+#define STAN_MCMC_MPI_VAR_ADAPTATION_HPP
+
+#include <stan/mcmc/cross_chain/mpi_metric_adaptation.hpp>
+#include <stan/math/prim/fun/Eigen.hpp>
+#include <vector>
+
+#ifdef STAN_LANG_MPI
+#include <stan/math/torsten/mpi/mpi_var_estimator.hpp>
+#endif
+
+namespace stan {
+
+namespace mcmc {
+
+  class mpi_var_adaptation : public mpi_metric_adaptation {
+#ifdef STAN_LANG_MPI
+  using est_t = stan::math::mpi::mpi_var_estimator;
+
+public:
+  std::vector<est_t> estimators;
+
+  mpi_var_adaptation() = default;
+
+  mpi_var_adaptation(int n_params, int max_num_windows)
+    : estimators(max_num_windows, est_t(n_params))
+  {}
+
+  mpi_var_adaptation(int n_params, int num_iterations, int window_size)
+    : estimators(num_iterations / window_size, est_t(n_params))
+  {}
+
+  virtual void add_sample(const Eigen::VectorXd& q, int curr_win_count) {
+    for (int win = 0; win < curr_win_count; ++win) {
+      estimators[win].add_sample(q);
+    }
+  }
+
+  virtual void learn_metric(Eigen::VectorXd& var, int win, int curr_win_count,
+                    const stan::math::mpi::Communicator& comm) {
+    learn_variance(var, win, curr_win_count, comm);
+  }
+
+  void learn_variance(Eigen::VectorXd& var, int win, int curr_win_count,
+                      const stan::math::mpi::Communicator& comm) {
+    double n = static_cast<double>(estimators[win].sample_variance(var, comm));
+    var = (n / (n + 5.0)) * var
+      + 1e-3 * (5.0 / (n + 5.0)) * Eigen::VectorXd::Ones(var.size());
+  }
+
+  virtual void restart() {
+    for (auto&& e : estimators) {
+      e.restart();
+    }
+  }
+
+#else
+  public:
+  mpi_var_adaptation(int n_params, int num_iterations, int window_size)
+    {}
+#endif
+};
+
+}  // namespace mcmc
+
+}  // namespace stan
+
+
+
+#endif
