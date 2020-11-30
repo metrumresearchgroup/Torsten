59bac81fdb380454c8751318ad99ad3c6279abff
Yi Zhang <yiz@metrumrg.com>
2020-11-30 13:11:03 -0800

    rm math

--- cmdstan/stan/lib/stan_math/test/unit/math/mix/core/operator_equal_test.cpp
+++
@@ -1,7 +0,0 @@
-#include <test/unit/math/test_ad.hpp>
-
-TEST(mathMixCore, operatorEqual) {
-  auto f = [](const auto& x1, const auto& x2) { return x1 == x2; };
-  stan::test::expect_common_comparison(f);
-  stan::test::expect_complex_common_comparison(f);
-}
--- cmdstan/stan/lib/stan_math/test/unit/math/mix/fun/inv_logit_test.cpp
+++
@@ -1,8 +0,0 @@
-#include <test/unit/math/test_ad.hpp>
-
-TEST(mathMixMatFun, invLogit) {
-  auto f = [](const auto& x1) { return stan::math::inv_logit(x1); };
-  stan::test::expect_common_unary_vectorized(f);
-  stan::test::expect_unary_vectorized(f, -2.6, -2, -1.2, -0.2, 0.5, 1, 1.3, 1.5,
-                                      3);
-}
