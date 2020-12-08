40937624be76e655082b31a170a9fd80616bae90
Yi Zhang <yiz@metrumrg.com>
2020-12-08 12:40:19 -0800

    rm math

--- cmdstan/stan/lib/stan_math/test/unit/math/mix/fun/fabs_test.cpp
+++
@@ -1,7 +0,0 @@
-#include <test/unit/math/test_ad.hpp>
-
-TEST(mathMixMatFun, fabs) {
-  auto f = [](const auto& x1) { return stan::math::fabs(x1); };
-  stan::test::expect_common_nonzero_unary_vectorized(f);
-  stan::test::expect_unary_vectorized(f, -2.6, -2, -0.5, 1.5, 2.0, 3);
-}
--- cmdstan/stan/lib/stan_math/test/unit/math/prim/fun/sqrt_test.cpp
+++
@@ -1,9 +0,0 @@
-#include <stan/math.hpp>
-#include <gtest/gtest.h>
-
-TEST(MathFunctions, sqrtInt) {
-  using stan::math::sqrt;
-  EXPECT_FLOAT_EQ(std::sqrt(3.0), sqrt(3));
-  EXPECT_FLOAT_EQ(std::sqrt(3.1), sqrt(3.1));
-  EXPECT_TRUE(stan::math::is_nan(sqrt(-2)));
-}
