a311c9d82e508e7520bc61fd069833b37f878db2
Yi Zhang <yzhang@c-path.org>
2023-12-30 04:50:50 -0800

    Squashed 'stanc3/' changes from 37ad076ae0..de0d67fceb
    de0d67fceb use torsten namespace for "pmx_" functions
    
    git-subtree-dir: stanc3
    git-subtree-split: de0d67fceb0bd37e8026b5f8f7ba21a235758161

--- src/stan_math_backend/Lower_expr.ml
+++ src/stan_math_backend/Lower_expr.ml
@@ -7,6 +7,7 @@ open Cpp
 
 let stan_namespace_qualify f =
   if String.is_suffix ~suffix:"functor__" f || String.contains f ':' then f
+  else if String.is_prefix ~prefix:"pmx_solve_" f || String.is_prefix ~prefix:"pmx_ode_" f then "torsten::" ^ f
   else "stan::math::" ^ f
 
 let fn_renames =
