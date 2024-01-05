65cb38118b16636f9d90cf939d825c9ac0b91126
Yi Zhang <yzhang@c-path.org>
2024-01-05 13:38:20 -0800

    update stanc3 binary tag

--- cmdstan/make/torsten_stanc.mk
+++ cmdstan/make/torsten_stanc.mk
@@ -1,5 +1,5 @@
 ifeq ($(STANC2),)
-TORSTEN_STANC3_VERSION=torsten_v0.91.0rc1
+TORSTEN_STANC3_VERSION=torsten_v0.91.0rc2
 
 # supply path to STANC3 to run custom stanc3 transpiler
 #  STANC3=../stanc3
