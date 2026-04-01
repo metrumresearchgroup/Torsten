62b96d7a6e9b9135f92009a5e7dbb0162a17cea3
Yi Zhang <yi.zhang6@iconplc.com>
2026-04-01 12:58:42 -0700

    Squashed 'cmdstan/' content from commit fe6f076fc2
    git-subtree-dir: cmdstan
    git-subtree-split: fe6f076fc2575bae04c37352376ddc369b0f7872

---
+++ src/cmdstan/arguments/arg_generate_quantities_fitted_params.hpp
@@ -0,0 +1,26 @@
+#ifndef CMDSTAN_ARGUMENTS_ARG_GENERATE_QUANTITIES_FITTED_PARAMS_HPP
+#define CMDSTAN_ARGUMENTS_ARG_GENERATE_QUANTITIES_FITTED_PARAMS_HPP
+
+#include <cmdstan/arguments/singleton_argument.hpp>
+
+namespace cmdstan {
+
+class arg_generate_quantities_fitted_params : public string_argument {
+ public:
+  arg_generate_quantities_fitted_params() : string_argument() {
+    _name = "fitted_params";
+    _description
+        = "Input file of sample of fitted parameter values for model "
+          "conditioned on data.  If multiple chains are run, this can either "
+          "be a single path, in which case its name will have _ID appended, or "
+          "a comma-separated list of names.";
+    _validity = "Path to existing file";
+    _default = "\"\"";
+    _default_value = "";
+    _value = _default_value;
+  }
+};
+
+}  // namespace cmdstan
+
+#endif
